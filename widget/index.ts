3368a5ab0714dd75519e370bcfed801b913c0d37
Ivan Krivonos <krivonos.ivann@gmail.com>
2021-01-12 22:59:02 +0300

    ~~

--- widget/index.ts
+++ widget/index.ts
@@ -1,3 +1,18 @@
-chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
-
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  // eslint-disable-next-line no-console
+  console.log(
+    sender.tab
+      ? `from a content script:${sender.tab.url}`
+      : "from the extension"
+  );
+  // eslint-disable-next-line no-console
+  console.log(request);
+  switch (request.type) {
+    case "SET_COLOR":
+      document.body.style.backgroundColor = request.color;
+      sendResponse(true);
+      break;
+    default:
+      break;
+  }
 });
